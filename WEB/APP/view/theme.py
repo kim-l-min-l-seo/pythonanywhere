6afb525cd9bb2a89585a9aa5c6f6effa45647e2b
김민서 <blackcode-@naver.com>
2022-02-22 01:44:51 +0900

    update theme.py

--- WEB/APP/view/theme.py
+++ WEB/APP/view/theme.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from setuptools import Require
 
 class Theme :
+    
     def theme(request, page):
         print("PAGE : ",page)
         page ='Home'
@@ -11,3 +13,6 @@ class Theme :
         
         if page == "index":   
             return render(request, './theme/index.html', context)
+        else : 
+            return HttpResponse("개발 진행중 2022.02.22")
+        
