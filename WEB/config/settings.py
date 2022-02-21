131aee224b5ac148286ad2f2c79697d101f9431d
김민서 <blackcode-@naver.com>
2022-02-22 00:08:44 +0900

    add theme

--- WEB/config/settings.py
+++ WEB/config/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'APP',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'APP',                          # 2022.02.21 APP 추가
 ]
 
 MIDDLEWARE = [
