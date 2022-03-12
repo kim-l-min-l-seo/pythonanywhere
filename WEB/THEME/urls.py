bc3caa0d3ffa6e3d19c945fc7af33b386b936d6a
김민서 <blackcode-@naver.com>
2022-03-13 03:52:29 +0900

    update WEB

--- WEB/THEME/urls.py
+++ WEB/THEME/urls.py
@@ -1,6 +1,8 @@
 import imp
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.conf.urls import url
+from django.views.static import serve
 
 from . import views
 
@@ -20,3 +22,4 @@ urlpatterns = [
     path('web/login/', auth_views.LoginView.as_view(template_name='web/index.html'), name='login'),
     path('web/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+urlpatterns += url(r'^static/(?P<path>.*)', serve, kwargs={'insecure': True})
