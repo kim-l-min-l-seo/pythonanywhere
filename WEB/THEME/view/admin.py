60d4a474ece0fd63fe2ef705e05d20a06ae32ee2
김민서 <blackcode-@naver.com>
2022-03-19 00:08:12 +0900

    update WEB

--- WEB/THEME/view/admin.py
+++ WEB/THEME/view/admin.py
@@ -5,72 +5,87 @@ from setuptools import Require
 class Theme :
     
     def url(request, menu, pages):
-        print("title : ",menu)
+        print("menu : ", menu)
         print("page : ",pages)
         
+        url = 'theme';
+        html = 'XXX1'
+        
         context = {
-            'url' : "theme",
+            'url': url,
+            'html' : html,
             'menu': menu,
             'pages': pages,
         }
         
+        if html == 'XXX1':
+            fileName = 'admin'
+        elif html == 'XX1X':
+            fileName = 'game'
+        elif html == 'XX11':
+            fileName = 'industrious'
+        
+        # HTML path = url/filename(html)/menu/pages
+        print("URL : ", './'+url+'/'+fileName+'/'+menu+'(2)/'+pages+'.html')
+        
         if (menu == "index" or menu == "0001") and pages == "01":
-            context = {'menu': menu,'title':'Alerts'}   
-            return render(request, './theme/admin/01/01.html', context)
+            context['title'] = 'Alerts'
+            return render(request, './'+url+'/'+fileName+'/01/01.html', context)
         elif menu == "0010" and pages == "01":
-            context = {'menu': menu,'title':'Profile'}
-            return render(request, './theme/admin/02/01.html', context)
+            context['title'] = 'Profile'
+            return render(request, './'+url+'/'+fileName+'/02/01.html', context)
         elif menu == "0011" and pages == "01":
-            context = {'menu': menu,'title':'Settings'}
-            return render(request, './theme/admin/03/01.html', context)
+            context['title'] = 'Settings'
+            return render(request, './'+url+'/'+fileName+'/03/01.html', context)
         elif menu == "0100" and pages == "01":
-            context = {'menu': menu,'title':'Invoice'}
-            return render(request, './theme/admin/04/01.html', context)
+            context['title'] = 'Invoice'
+            return render(request, './'+url+'/'+fileName+'/04/01.html', context)
         elif menu == "0101" and pages == "01":
-            context = {'menu': menu,'title':'Blank'}
-            return render(request, './theme/admin/05/01.html', context)
+            context['title'] = 'Blank'
+            return render(request, './'+url+'/'+fileName+'/05/01.html', context)
         elif menu == "0110":
             if pages == "01":
-                context = {'menu': menu,'title':'Alerts'}
-                return render(request, './theme/admin/06/01.html', context)
+                context['title'] = 'Alerts'
+                return render(request, './'+url+'/'+fileName+'/06/01.html', context)
             elif pages == "02":
-                context = {'menu': menu,'title':'Buttons'}
-                return render(request, './theme/admin/06/02.html', context)
+                context['title'] = 'Buttons'
+                return render(request, './'+url+'/'+fileName+'/06/02.html', context)
             elif pages == "03":
-                context = {'menu': menu,'title':'Cards'}
-                return render(request, './theme/admin/06/03.html', context)
+                context['title'] = 'Cards'
+                return render(request, './'+url+'/'+fileName+'/06/03.html', context)
             elif pages == "04":
-                context = {'menu': menu,'title':'General'}
-                return render(request, './theme/admin/06/04.html', context)
+                context['title'] = 'General'
+                return render(request, './'+url+'/'+fileName+'/06/04.html', context)
             elif pages == "05":
-                context = {'menu': menu,'title':'Grid'}
-                return render(request, './theme/admin/06/05.html', context)
-            elif pages == "06":                
-                context = {'menu': menu,'title':'Modals'}
-                return render(request, './theme/admin/06/06.html', context)
+                context['title'] = 'Grid'
+                return render(request, './'+url+'/'+fileName+'/06/05.html', context)
+            elif pages == "06":
+                context['title'] = 'Modals'
+                return render(request, './'+url+'/'+fileName+'/06/06.html', context)
             elif pages == "07":
-                context = {'menu': menu,'title':'Typography'}
-                return render(request, './theme/admin/06/07.html', context)
+                context['title'] = 'Typography'
+                return render(request, './'+url+'/'+fileName+'/06/07.html', context)
         elif menu == "0111" and pages == "01":
-            context = {'menu': menu,'title':'Feather'}
-            return render(request, './theme/admin/07/01.html', context)
+            context['title'] = 'Feather'
+            return render(request, './'+url+'/'+fileName+'/07/01.html', context)
         elif menu == "1000": 
             if pages == "01":
-                context = {'menu': menu,'title':'Form Layouts'}
-                return render(request, './theme/admin/08/01.html', context)
+                context['title'] = 'Form Layouts'
+                return render(request, './'+url+'/'+fileName+'/08/01.html', context)
             if pages == "02":
-                context = {'menu': menu,'title':'Basic Inputs'}
-                return render(request, './theme/admin/08/02.html', context)
+                context['title'] = 'Basic Inputs'
+                return render(request, './'+url+'/'+fileName+'/08/02.html', context)
         elif menu == "1001" and pages == "01":
-            context = {'menu': menu,'title':'Basic Tables'}
-            return render(request, './theme/admin/09/01.html', context)
+            context['title'] = 'Basic Tables'
+            return render(request, './'+url+'/'+fileName+'/09/01.html', context)
         elif menu == "1010" and pages == "01":
-            context = {'menu': menu,'title':'Chart.js'}
-            return render(request, './theme/admin/10/01.html', context)
+            context['title'] = 'Chart.js'
+            return render(request, './'+url+'/'+fileName+'/10/01.html', context)
         elif menu == "1011" and pages == "01":
-            context = {'menu': menu,'title':'Google Maps'}
-            return render(request, './theme/admin/11/01.html', context)
+            context['title'] = 'Google Maps'
+            return render(request, './'+url+'/'+fileName+'/11/01.html', context)
         else : 
             # return HttpResponse("개발 진행중 2022.02.22 "+menu)
-            return render(request, './theme/admin/404.html', context)
+            context['title'] = 'Feather'
+            return render(request, './'+url+'/'+fileName+'/404.html', context)
         
--- WEB/THEME/view/industrious.py
+++ WEB/THEME/view/industrious.py
@@ -5,26 +5,33 @@ from setuptools import Require
 class Theme :
     
     def url(request, menu, pages):
-        print("title : ",menu)
+        url = 'theme'
+        print("menu : ", menu)
         print("page : ",pages)
         
-        context = {
-            'url' : "theme",
-            'menu': menu,
-            'pages': pages,
-        }
-        
         if (menu == "index" or menu == "0001") and pages == "01":
-            context = {'menu': menu,'title':'Industrious by TEMPLATED'}   
-            return render(request, './theme/industrious/index.html', context)
+            context = {
+                'url': url, 
+                'menu': menu,
+                'title': ''
+            }
+            return render(request, './'+url+'/industrious/index.html', context)
         elif menu == "0010" and pages == "01":
-            context = {'menu': menu,'title':'Generic Page - Industrious by TEMPLATED'}
-            return render(request, './theme/industrious/generic.html', context)
+            context = {
+                'url' : url,
+                'menu': menu,
+                'title':'Generic Page'
+            }
+            return render(request, './'+url+'/industrious/generic.html', context)
         elif menu == "0011" and pages == "01":
-            context = {'menu': menu,'title':'Elements - Industrious by TEMPLATED'}
-            return render(request, './theme/industrious/elements.html', context)
+            context = {
+                'url' : url,
+                'menu': menu,
+                'title':'Elements'
+            }
+            return render(request, './'+url+'/industrious/elements.html', context)
         
         else : 
             # return HttpResponse("개발 진행중 2022.02.22 "+menu)
-            return render(request, './theme/404.html', context)
+            return render(request, './'+url+'/404.html', context)
         
