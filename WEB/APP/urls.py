5836a6ae023efe7e712f53b90b791c9c4109ea01
김민서 <blackcode-@naver.com>
2022-03-28 01:10:06 +0900

    update WEB 2-10 폼 END

--- WEB/APP/urls.py
+++ WEB/APP/urls.py
@@ -4,15 +4,20 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 from .view.web import Web
-from .view import pybo
+from .view.pybo import pybo, forms
 
 app_name = 'APP'
 
 urlpatterns = [
-    path('pybo/', pybo.index),
-    path('pybo/<int:question_id>/', pybo.detail),
-    
     path('', views.index),
+    
+    # ============ pybo ============ #
+    path('pybo/',                                   pybo.index,             name='pybo_index'),
+    path('pybo/<int:question_id>/',                 pybo.detail,            name='pybo_detail'),
+    path('pybo/answer/create/<int:question_id>/',   pybo.answer_create,     name='pybo_answer_create'),
+    path('pybo/question/create/',                   pybo.question_create,   name='pybo_question_create'),
+    
+    # ============ web  ============ #
     path('web/<menu>/<pages>/', Web.url),
     path('web/login/',  auth_views.LoginView.as_view(template_name='web/game/index.html'),  name='game_login'),
     path('web/logout/', auth_views.LogoutView.as_view(),                                    name='game_logout'),
---
+++ WEB/APP/view/pybo/pybo.py
@@ -0,0 +1,58 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from setuptools import Require
+from ...models import Question, Answer
+from .forms import QuestionForm, AnswerForm
+
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    # question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    # return redirect('APP:pybo_detail', question_id=question.id)
+    return render(request, 'pybo/question_detail.html', context) 
+
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('APP:pybo_index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
