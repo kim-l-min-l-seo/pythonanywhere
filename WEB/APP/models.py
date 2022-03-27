71c4e198c6417137cc8ce4fc82032a083378f2f7
김민서 <blackcode-@naver.com>
2022-03-27 22:46:52 +0900

    update WEB

--- WEB/APP/models.py
+++ WEB/APP/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200)  #   제목
+    content = models.TextField()                #   내용
+    create_date = models.DateTimeField()        #   작성일시
+
+    def __str__(self):
+        return self.subject
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)    # PK
+    content = models.TextField()                                        # 답변내용
+    create_date = models.DateTimeField()                                # 답변일시
--- WEB/APP/view/web.py
+++ WEB/APP/view/web.py
@@ -15,26 +15,26 @@ class Web :
         if (menu == "index" or menu == "home") and pages == "x" or (menu == "login" or menu == "logout") and pages == "success":   
             return render(request, './web/game/index.html', context)
         elif menu == "main" and pages == "x":
-            return render(request, './web/main.html', context)
+            return render(request, './web/game/main.html', context)
         elif menu == "pages" :
             if pages == "1" :
-                return render(request, './web/'+menu+'/1_categories-list.html', context)
+                return render(request, './web/game/'+menu+'/1_categories-list.html', context)
             elif pages == "2" :
-                return render(request, './web/'+menu+'/2_categories-grid.html', context)
+                return render(request, './web/game/'+menu+'/2_categories-grid.html', context)
             elif pages == "3" :
-                return render(request, './web/'+menu+'/3_typography.html', context)
+                return render(request, './web/game/'+menu+'/3_typography.html', context)
             elif pages == "4" :
-                return render(request, './web/'+menu+'/4_details-post-default.html', context)
+                return render(request, './web/game/'+menu+'/4_details-post-default.html', context)
             elif pages == "5" :
-                return render(request, './web/'+menu+'/5_details-post-gallery.html', context)
+                return render(request, './web/game/'+menu+'/5_details-post-gallery.html', context)
             elif pages == "6" :
-                return render(request, './web/'+menu+'/6_details-post-review.html', context)
+                return render(request, './web/game/'+menu+'/6_details-post-review.html', context)
             elif pages == "7" :
-                return render(request, './web/'+menu+'/7_contact.html', context)
+                return render(request, './web/game/'+menu+'/7_contact.html', context)
         elif menu == "JavaScript" :
             if pages == "1":
-                   return render(request, './web/'+menu+'/1_jquery.html', context)
+                   return render(request, './web/game/'+menu+'/1_jquery.html', context)
       
         else : 
             # return HttpResponse("개발 진행중 2022.02.22 "+menu)
-            return render(request, './web/404.html', context)
+            return render(request, './web/game/404.html', context)
