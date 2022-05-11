1f63bfdb2885598329e5b4675d34725cd35f9ad0
김민서 <blackcode-@naver.com>
2022-05-12 01:17:02 +0900

    update web XX11

--- WEB/APP/view/industrious.py
+++ WEB/APP/view/industrious.py
@@ -5,31 +5,42 @@ from setuptools import Require
 class Web :
     
     def url(request, menu, pages):
-        url = 'theme'
+        url = 'BLACKCODE'
+        web = 'XX11'
         print("menu : ", menu)
         print("page : ",pages)
         
-        if (menu == "index" or menu == "0001") and pages == "01":
+        if (menu == "index" or menu == "0000") and pages == "XX":
             context = {
                 'url': url, 
                 'menu': menu,
                 'title': ''
             }
-            return render(request, './'+url+'/industrious/index.html', context)
-        elif menu == "0010" and pages == "01":
+            return render(request, './'+url+'/'+web+'/0000_index.html', context)
+        elif menu == "0010" and pages == "XX":
             context = {
                 'url' : url,
                 'menu': menu,
                 'title':'Generic Page'
             }
-            return render(request, './'+url+'/industrious/generic.html', context)
-        elif menu == "0011" and pages == "01":
+            return render(request, './'+url+'/'+web+'/generic.html', context)
+        
+        
+        
+        elif (menu == "ReleaseNote" or menu == "1111") and pages == "XX":
+            context = {
+                'url' : url,
+                'menu': menu,
+                'title':'Release Note'
+            }
+            return render(request, './'+url+'/'+web+'/1111_Release Note.html', context)
+        elif menu == "9999" and pages == "XX":
             context = {
                 'url' : url,
                 'menu': menu,
                 'title':'Elements'
             }
-            return render(request, './'+url+'/industrious/elements.html', context)
+            return render(request, './'+url+'/'+web+'/elements.html', context)
         
         else : 
             # return HttpResponse("개발 진행중 2022.02.22 "+menu)
