df091e3f5917c438c662f743249acc297f7cf466
김민서 <blackcode-@naver.com>
2022-07-03 03:00:53 +0900

    update setting

--- WEB/config/settings.py
+++ WEB/config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0&y#w=v_)upd1kfw7y!#0e_^cg6*17+q8=z1@+5yhdwa#x*np#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 # 2022.02.22 ALLOWED_HOSTS 설정, pythonanywhere 추가
 ALLOWED_HOSTS = ['*']
@@ -80,17 +80,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.parent / 'WEB/db.sqlite3',
+        # 'NAME': Path(__file__).resolve().parent / 'WEB/db.sqlite3',
     }
 }
 # 배포용
-import os
-DATABASES={
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES={
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -134,11 +134,11 @@ STATIC_URL = '/static/'
 # 2022.02.22 static 디렉토리 추가
 import os
 # STATIC_ROOT = BASE_DIR / 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-#     os.path.join(BASE_DIR,"static")
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    os.path.join(BASE_DIR,"static")
+]
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
